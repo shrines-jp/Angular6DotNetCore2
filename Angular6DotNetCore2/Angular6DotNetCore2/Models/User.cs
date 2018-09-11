7c27f77dad8ce349bfc7fa851a6cb55d65213a6c
shrines-jp <shrines@outlook.jp>
2018-09-11 20:18:23 +0900

    MongoDB 追加

---
+++ Angular6DotNetCore2/Angular6DotNetCore2/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Angular6DotNetCore2.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        [BsonElement("last_name")]
+        public string LastName { get; set; }
+        [BsonElement("first_name")]
+        public string FirstName { get; set; }        
+    }
+}
